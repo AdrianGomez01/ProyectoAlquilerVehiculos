b149345a601bd48ce875f74c419dea82ea759c3b
Desi <desixd107@gmail.com>
2024-03-05 14:30:08 +0100

    script para añadir registros a la tabla

---
+++ INSERT INTO conductores.sql
@@ -0,0 +1,6 @@
+INSERT INTO conductores (nombre, ap1, ap2, DNI, tipo_carnet, anios_carnet) VALUES
+('Juan', 'Perez', 'Gomez', '12345678A', 'B', 5),
+('María', 'López', 'García', '87654321B', 'B', 8),
+('Pedro', 'Martínez', 'Sánchez', '98765432C', 'B', 3),
+('Ana', 'González', 'Fernández', '54321678D', 'B', 6),
+('Luis', 'Sánchez', 'Martínez', '45678912E', 'B', 4);
---
+++ INSERT INTO vehiculos.sql
@@ -0,0 +1,6 @@
+INSERT INTO vehiculos (marca, modelo, tipo, matricula, kilometrajecoche) VALUES
+('Toyota', 'Corolla', 'Sedán', 'ABC123', 15000),
+('Honda', 'Civic', 'Sedán', 'DEF456', 20000),
+('Ford', 'Focus', 'Hatchback', 'GHI789', 18000),
+('Chevrolet', 'Cruze', 'Sedán', 'JKL012', 22000),
+('Volkswagen', 'Golf', 'Hatchback', 'MNO345', 19000);
