22a48a21388e916ddeba1eec8967dd0dc7922bfb
Adrian Gomez <adrian.gomez.garlito@gmail.com>
2024-03-05 14:07:18 +0100

    Initial commit

---
+++ src/main/java/org/example/proyectoalquilervehiculos/ProyectoAlquilerVehiculosApplication.java
@@ -0,0 +1,13 @@
+package org.example.proyectoalquilervehiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoAlquilerVehiculosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoAlquilerVehiculosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/proyectoalquilervehiculos/ProyectoAlquilerVehiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.proyectoalquilervehiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoAlquilerVehiculosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
