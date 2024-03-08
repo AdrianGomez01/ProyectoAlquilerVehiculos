04f3444945f4cf61884d79d9e27a19a4e136e24a
Desi <desixd107@gmail.com>
2024-03-08 08:52:57 +0100

    script para añadir registros a la tabla

---
+++ src/main/java/org/example/proyectoalquilervehiculos/ServicioRepository.java
@@ -0,0 +1,8 @@
+package org.example.proyectoalquilervehiculos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public class ServicioRepository extends JpaRepository<, Long> {
+
+}
